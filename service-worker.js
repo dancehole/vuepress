809cfb1f0f43eeca22ecaefe323f51022dbc6e0d
dancehole <31036419+dancehole@users.noreply.github.com>
2024-08-30 12:45:31 +0000

    Deploying to gh-pages from @ dancehole/vuepress@ce05e7528eb8323849bb2c11e80916bd91a1ea27 🚀

---
+++ service-worker.js
@@ -0,0 +1,177 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "756ff30ba6fba65504b5e3a67d851244"
+  },
+  {
+    "url": "assets/css/0.styles.f0d21de3.css",
+    "revision": "80f60e0b29b62dcc1788ba125653ff35"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/1.8b096960.js",
+    "revision": "da777aaeea628431216c38e49d99a61a"
+  },
+  {
+    "url": "assets/js/10.cff2be69.js",
+    "revision": "e85cd86278bcb9cbd31c2ba1fd126d75"
+  },
+  {
+    "url": "assets/js/11.ff2b69b4.js",
+    "revision": "e1f9d630f6d634c1617925ae0930e3c7"
+  },
+  {
+    "url": "assets/js/12.3bfba831.js",
+    "revision": "7dc92278257359c45dab7ab719354bbc"
+  },
+  {
+    "url": "assets/js/13.e431ba5f.js",
+    "revision": "1e08a77cbe3ab6dc2fdf21275fcd0c31"
+  },
+  {
+    "url": "assets/js/14.139d1c2e.js",
+    "revision": "ea615551ddb7d9b90ed0bfebcf9c7217"
+  },
+  {
+    "url": "assets/js/15.09fd187b.js",
+    "revision": "2afc31e5d746fc10818add28a1e2d46f"
+  },
+  {
+    "url": "assets/js/16.d2ac81b3.js",
+    "revision": "0d1647b1e70dc1798d13c3f71e014f3a"
+  },
+  {
+    "url": "assets/js/17.2d689134.js",
+    "revision": "a6faed2f6c107505c975f04e3399dc28"
+  },
+  {
+    "url": "assets/js/18.7e53a849.js",
+    "revision": "079cf6ddcae6aaa7727c2ee6b64efa5c"
+  },
+  {
+    "url": "assets/js/19.3939804d.js",
+    "revision": "a11292efa9a6e98e406067d85ad97a7c"
+  },
+  {
+    "url": "assets/js/2.ecaa54b6.js",
+    "revision": "53bae9ba55d121ca8943740013b5681c"
+  },
+  {
+    "url": "assets/js/20.ce47bf4e.js",
+    "revision": "efe6bfe69df0cbbaa0d2869735490846"
+  },
+  {
+    "url": "assets/js/21.cbd0d2e2.js",
+    "revision": "bbc1b0e2134348b68ec91d6fe931048d"
+  },
+  {
+    "url": "assets/js/22.1eeb5ac8.js",
+    "revision": "48482ba9194e66795004b5bf05127f93"
+  },
+  {
+    "url": "assets/js/23.391a3103.js",
+    "revision": "20ea43769d10177dd101343f7dba62cb"
+  },
+  {
+    "url": "assets/js/24.2ad1f624.js",
+    "revision": "fa90dfc2b5605bcca9f754367746d9e6"
+  },
+  {
+    "url": "assets/js/25.2981dd7e.js",
+    "revision": "3a5408282cf60d6c4947647ff1b303ae"
+  },
+  {
+    "url": "assets/js/26.0977d4e7.js",
+    "revision": "9cdf5e9dadc6760d956e0293857abce2"
+  },
+  {
+    "url": "assets/js/27.a66d6abb.js",
+    "revision": "049a8373c11073902f5ff12aeedd71a8"
+  },
+  {
+    "url": "assets/js/3.99cb1f85.js",
+    "revision": "84aafb1b8f416eabf89dd9519d36ad9c"
+  },
+  {
+    "url": "assets/js/4.00b2527c.js",
+    "revision": "5c95b31cd1c0bcc5b17d3785c22e0d19"
+  },
+  {
+    "url": "assets/js/5.3b1b9673.js",
+    "revision": "17f3b84fe3470b592562be72d99b8d09"
+  },
+  {
+    "url": "assets/js/6.75afbc57.js",
+    "revision": "7e61f368f63228b08eab5f555b4875c1"
+  },
+  {
+    "url": "assets/js/7.007b7e70.js",
+    "revision": "3d69d7047a111ef22e55b5ec73129ea7"
+  },
+  {
+    "url": "assets/js/app.9f7c6cc5.js",
+    "revision": "8af52f99e475b93b6b1f735c428a2df8"
+  },
+  {
+    "url": "assets/js/vendors~docsearch.4ad8f938.js",
+    "revision": "10fc9a7c7b96692c18757b3caab9f1fb"
+  },
+  {
+    "url": "guide/VuePress创建文档网站.html",
+    "revision": "603c8e4cf91122217136b11cbc10927e"
+  },
+  {
+    "url": "images/book.jpg",
+    "revision": "31a8c253a9dce15d1d4b2850d86c4f41"
+  },
+  {
+    "url": "index.html",
+    "revision": "e3e368db3d73196ee9a31b198379569f"
+  },
+  {
+    "url": "link/contact.html",
+    "revision": "ae18981d6c335a7afc8cffd42f1a2d90"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
